004956add1529bc64cea988f0e632b6bbbcdf9f4
Ivan Khokhlov <khokhlov.ivan@gmail.com>
2021-07-14 00:23:54 +0200

    Fixed create_tables.py but not recommended

--- create_tables.py
+++ create_tables.py
@@ -1,58 +1,66 @@
-import configparser
 import psycopg2
-from sql_queries import create_staging_table_queries, drop_staging_table_queries, \
-    create_analytical_table_queries, drop_analytical_table_queries
+import logging
+import configparser
+from redshift import RedshiftManager
 
-config = configparser.ConfigParser()
-config.read_file(open('dwh.cfg'))
+logging.basicConfig(level=logging.INFO)
 
-DWH_HOST            = config.get('CLUSTER', 'DWH_HOST')
-DWH_DB              = config.get('CLUSTER', 'DWH_DB')
-DWH_DB_USER         = config.get('CLUSTER', 'DWH_DB_USER')
-DWH_DB_PASSWORD     = config.get('CLUSTER', 'DWH_DB_PASSWORD')
-DWH_PORT            = config.get('CLUSTER', 'DWH_PORT')
+def open_db_connection():
+    """
+    Creates a connection to Redshift based on params in the config file.
+    """
+    config = configparser.ConfigParser()
+    config.read_file(open('dwh.cfg'))
 
-
-def drop_staging_tables(cur, conn):
-    for query in drop_staging_table_queries:
-        cur.execute(query)
-        conn.commit()
-
-def drop_analytical_tables(cur, conn):
-    for query in drop_analytical_table_queries:
-        cur.execute(query)
-        conn.commit()
-
-
-def create_staging_tables(cur, conn):
-    for query in create_staging_table_queries:
-        print(f"Running query: {query}")
-        cur.execute(query)
-        conn.commit()
+    DWH_HOST            = config.get('CLUSTER', 'DWH_HOST')
+    DWH_DB              = config.get('CLUSTER', 'DWH_DB')  
+    DWH_DB_USER         = config.get('CLUSTER', 'DWH_DB_USER')
+    DWH_DB_PASSWORD     = config.get('CLUSTER', 'DWH_DB_PASSWORD')
+    DWH_PORT            = config.get('CLUSTER', 'DWH_PORT')
+    
+    try:
+        conn = psycopg2.connect(f"""
+            host={DWH_HOST} dbname={DWH_DB} user={DWH_DB_USER} 
+            password={DWH_DB_PASSWORD} port={DWH_PORT}
+            """)
+        cur = conn.cursor()
+        logging.info("Connected to Redshift.")
+        return conn, cur
+    except Exception as e:
+        logging.warning(f"Could not connect to Redshift. Error: {e}")
         
-def create_analytical_tables(cur, conn):
-    for query in create_analytical_table_queries:
-        print(f"Running query: {query}")
-        cur.execute(query)
-        conn.commit()
-
+def close_db_connection(conn):
+    """
+    Closes Redshift connection.
+    """
+    conn.close()
+    logging.info("Redshift connection closed.")
 
 def main():
-    conn = psycopg2.connect(f"""
-        host={DWH_HOST} dbname={DWH_DB} user={DWH_DB_USER} 
-        password={DWH_DB_PASSWORD} port={DWH_PORT}
-        """)
-    if conn:
-        print("Connected to redshift!")
-    cur = conn.cursor()
+    """
+    Drops all tables in Redshift and then creates them again.
+    
+    I recommend not using create_tables.py and instead running etl.py
+    because that will also provision the infrastructure.
+    """
+    logging.info('Starting job.')
 
-    #drop_staging_tables(cur, conn)
-    #create_staging_tables(cur, conn)
-    drop_analytical_tables(cur, conn)
-    create_analytical_tables(cur, conn)
-
-    conn.close()
-
+    # Create database connection
+    conn, cur = open_db_connection()
+    
+    # Setup Redshift tables
+    redshift = RedshiftManager(conn, cur)
+    logging.info('Creating Redshift tables...')
+    redshift.drop_analytical_tables()
+    redshift.drop_staging_tables()
+    redshift.create_staging_tables()
+    redshift.create_analytical_tables()
+    logging.info('Tables have been created!')
+        
+    # Close database connection
+    close_db_connection(conn)
+    
+    logging.info('Job complete.')
 
 if __name__ == "__main__":
     main()
